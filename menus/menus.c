032472d258575586ea937d762e83b41fcb927d28
PauloDev <paulodouglasmdias@gmail.com>
2024-09-23 18:03:40 -0300

    feat: cria o menu principal do projeto #8

---
+++ menus/menus.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void menuPrincipal(void) {
+    printf("\n");
+    printf("---------------------------------------------------------------------\n");
+    printf("|                                                                   |\n");
+    printf("|           Sig-task: Sua genda de Tarefas e Compromissos           |\n");
+    printf("|                                                                   |\n");
+    printf("---------------------------------------------------------------------\n");
+    printf("|                                                                   |\n");
+    printf("|                           [1] Usuários                            |\n");
+    printf("|                           [2] Gerir Equipes                       |\n");
+    printf("|                           [3] Agenda                              |\n");
+    printf("|                           [4] Tarefas                             |\n");
+    printf("|                           [5] Compromissos                        |\n");
+    printf("|                           [0] Sair                                |\n");
+    printf("|                                                                   |\n");
+    printf("---------------------------------------------------------------------\n");
+}
