9fe9616eba79fd923ee032ccac053d6d2df9cf7c
André Leandro <raquel.am12@gmail.com>
2024-09-30 09:06:17 -0300

    feat: cria a tela principal do módulo tarefas #11

---
+++ Tarefas/TarefasView.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void tela_tarefas(void) {
+    printf("\n");
+    printf("---------------------------------------------------\n");
+    printf("|                      Tarefas                    |\n");
+    printf("---------------------------------------------------\n");
+    printf("|               [1] Cadastrar Tarefas             |\n");
+    printf("|               [2] Exibir Trefas                 |\n");
+    printf("|               [3] Editar Tarefas                |\n");
+    printf("|               [4] Excluir Tarefas               |\n");
+    printf("|               [0] Sair                          |\n");
+    printf("--------------------------------------------------\n");
+}
+
+
