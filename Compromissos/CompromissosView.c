75ecfae894529a70d299d670ee426a5fd00aebd7
André Leandro <raquel.am12@gmail.com>
2024-10-07 20:43:04 -0300

    feat: cria a tela de cadastro de compromissos #25

---
+++ Compromissos/CompromissosView.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "TarefasView.h"
+
+void menu_compromissos(void) {
+    printf("\n");
+    printf("---------------------------------------------------\n");
+    printf("|                    Compromissos                  |\n");
+    printf("---------------------------------------------------\n");
+    printf("|               [1] Cadastrar Compromisso          |\n");
+    printf("|               [2] Exibir Compromisso             |\n");
+    printf("|               [3] Editar Compromisso             |\n");
+    printf("|               [4] Excluir Compromisso            |\n");
+    printf("|               [0] Sair                           |\n");
+    printf("--------------------------------------------------\n");
+
+}
