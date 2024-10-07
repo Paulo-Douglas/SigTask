4de02b638a31f56f4fa998fccc4940291fd82e0e
PauloDev <paulodouglasmdias@gmail.com>
2024-10-07 19:43:32 -0300

    feat: adiciona as telas de tarefas no main

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "Menus/Menus.h"
 #include "Usuarios/UsuariosView.h"
+#include "Tarefas/TarefasView.h"
 
 int main(void) {
     menuPrincipal();
@@ -8,6 +9,13 @@ int main(void) {
     exibir_dados();
     alterar_dados();
     excluir_usuario();
+    menu_tarefas();
+    cadastrar_tarefas();
+    exibir_tarefas();
+    editar_tarefas();
+    excluir_tarefas();
+    dados_tarefas();
+    alterar_dados();
     menuSobre();
     return 0;
 }
