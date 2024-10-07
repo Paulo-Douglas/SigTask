5170784a89933cb50c9234e185fb3c3cb71c330f
PauloDev <paulodouglasmdias@gmail.com>
2024-10-07 19:33:01 -0300

    feat: cria o arquivo de cabeçalho com as assinaturas de TarefasView.c #11

---
+++ Tarefas/TarefasView.h
@@ -0,0 +1,12 @@
+#ifndef TAREFASVIEW
+#define TAREFASVIEW
+
+void menu_tarefas(void);
+void cadastrar_tarefas(void);
+void exibir_tarefas(void);
+void editar_tarefas(void);
+void excluir_tarefas(void);
+void dados_tarefas(void);
+void alterar_dados(void);
+
+#endif
