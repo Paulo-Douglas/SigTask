ac56ec0ff540c5fe01a24f0f43a48021e168811b
PauloDev <paulodouglasmdias@gmail.com>
2024-10-07 21:21:24 -0300

    chore: corrige erros de tabulação no menu compromissos #25

--- Compromissos/CompromissosView.c
+++ Compromissos/CompromissosView.c
@@ -3,15 +3,15 @@
 
 void menu_compromissos(void) {
     printf("\n");
-    printf("---------------------------------------------------\n");
-    printf("|                    Compromissos                  |\n");
-    printf("---------------------------------------------------\n");
+    printf("----------------------------------------------------\n");
+    printf("|                     Compromissos                 |\n");
+    printf("----------------------------------------------------\n");
     printf("|               [1] Cadastrar Compromisso          |\n");
     printf("|               [2] Exibir Compromisso             |\n");
     printf("|               [3] Editar Compromisso             |\n");
     printf("|               [4] Excluir Compromisso            |\n");
     printf("|               [0] Sair                           |\n");
-    printf("--------------------------------------------------\n");
+    printf("----------------------------------------------------\n");
 
 }
 
