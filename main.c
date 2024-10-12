c5f9b8994853a6a54246b3e5454f0e5d81bcedb5
PauloDev <paulodouglasmdias@gmail.com>
2024-10-12 14:52:54 -0300

    refactor: remove importações dos módulos e adicionar função de navegação principal #27

--- main.c
+++ main.c
@@ -1,29 +1,6 @@
-#include "Menus/Menus.h"
-#include "Usuarios/UsuariosView.h"
-#include "Tarefas/TarefasView.h"
-#include "Compromissos/CompromissosView.h"
+#include "Menus/NavigationController.h"
 
 int main(void) {
-    menuPrincipal();
-    menu_usuarios();
-    cadastrar_usuarios();
-    exibir_dados_usuario();
-    alterar_dados_usuario();
-    excluir_usuario();
-    menu_tarefas();
-    cadastrar_tarefas();
-    exibir_tarefas();
-    editar_tarefas();
-    excluir_tarefas();
-    dados_tarefas();
-    alterar_dados_tarefas();
-    menu_compromissos();
-    cadastrar_compromissos();
-    exibir_compromissos();
-    editar_compromissos();
-    excluir_compromissos();
-    dados_compromissos();
-    alterar_dados_compromissos();
-    menuSobre();
+    navegacao_modulos();
     return 0;
 }
