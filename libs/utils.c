d1ea3034bf9d0e1654af38702197619f50443afd
PauloDev <paulodouglasmdias@gmail.com>
2024-10-12 15:42:52 -0300

    refactor: cria a função de exibir menssagem e aguardar a entrada do usuário

--- libs/utils.c
+++ libs/utils.c
@@ -3,6 +3,13 @@
 void limpa_buffer(void) {
     int b;
     while((b = getchar()) b != '\n' && b != EOF) {
-        
+
     }
+}
+
+void exibir_menssagem(char *menssagem) {
+    printf("%s\n\n", menssagem);
+    printf("Tecle <ENTER> para continuar...")
+    getchar();
+    limpa_buffer();
 }
