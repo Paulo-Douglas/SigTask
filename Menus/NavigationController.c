26c75f0844f30ceb9d53a62aa58e07f165bc0d1c
PauloDev <paulodouglasmdias@gmail.com>
2024-10-12 16:21:46 -0300

    feat: implementa a função limpa_buffer na função de navegação entre módulos #27

--- Menus/NavigationController.c
+++ Menus/NavigationController.c
@@ -11,21 +11,27 @@ void navegacao_modulos(void) {
         switch (op){
             case '1':
                 menu_usuarios();
+                limpa_buffer();
                 break;
             case '2':
                 cadastrar_compromissos(); // colocar o de equipes aqui
+                limpa_buffer();
                 break;
             case '3':
                 alterar_dados_tarefas(); // colocar o de agenda aqui
+                limpa_buffer();
                 break;
             case '4':
                 menu_tarefas();
+                limpa_buffer();
                 break;
             case '5':
                 menu_compromissos();
+                limpa_buffer();
                 break;
             case '6':
                 menuSobre();
+                limpa_buffer();
                 break;
             case '0':
                 printf("Saindo...\n");
