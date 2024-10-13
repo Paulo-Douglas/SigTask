3da5d741723ca0c7ae61deb3cddfe670dfb2c6a5
André Leandro <raquel.am12@gmail.com>
2024-10-13 15:42:28 -0300

    feat: cria a tela principal dos módulos equipe #35

---
+++ Equipes/EquipesView.c
@@ -0,0 +1,13 @@
+void menu_equipes(void) {
+    printf("\n");
+    printf("----------------------------------------------------\n");
+    printf("|                       Equipes                    |\n");
+    printf("----------------------------------------------------\n");
+    printf("|               [1] Cadastrar Equipe               |\n");
+    printf("|               [2] Exibir Equipe                  |\n");
+    printf("|               [3] Editar Equipe                  |\n");
+    printf("|               [4] Excluir Equipe                 |\n");
+    printf("|               [0] Sair                           |\n");
+    printf("----------------------------------------------------\n");
+
+}
