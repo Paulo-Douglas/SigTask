c564a39406cc42eed05fd34150e256885d001353
André Leandro <speykergamer@gmail.com>
2024-10-13 15:59:35 -0300

    feat: cria arquivo de cabeçalho com as assinaturas do arquivo EquipesView.c #35

---
+++ Equipes/EquipesView.h
@@ -0,0 +1,13 @@
+#ifndef EQUIPESVIEW
+#define EQUIPESVIEW
+
+void menu_equipes(void);
+void cadastrar_equipe_academica(void);
+void cadastrar_equipe_empresarial(void);
+void exibir_equipe(void);
+void editar_equipe(void);
+void excluir_equipe(void);
+void alterar_equipe_academica(void);
+void alterar_equipe_empresarial(void);
+
+#endif
