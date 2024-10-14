a998a6f4ba00f2aac78c048bf6a990e49892852e
PauloDev <paulodouglasmdias@gmail.com>
2024-10-14 20:05:45 -0300

    feat: cria variáveis globais para auxiliar na navegação entre telas #44

--- Tarefas/TarefasView.c
+++ Tarefas/TarefasView.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "TarefasView.h"
 
+char titulo[25];
+char descricao[40];
+char data[10];
+char turno[10];
+char prioridade[18];
+char status[10];
+
 char menu_tarefas(void) {
     char op;
     printf("\n");
@@ -25,6 +32,23 @@ void cadastrar_tarefas(void) {
     printf("-----------------------------------------------------------------------\n");
     printf("|  Título  |  Descrição  |  Data  |  Turno  |  Prioridade  |  Status  |\n");
     printf("-----------------------------------------------------------------------\n");
+    printf("Informe o titulo dessa tarefa: ");
+    scanf("%25", titulo);
+    printf("\n");
+    printf("Informe a descrição dessa tarefa: ");
+    scanf("%40", descricao);
+    printf("\n");
+    printf("Informe a data para realizar essa tarefa (xx/xx/xxxx): ");
+    scanf("%10", data);
+    printf("\n");
+    printf("Informe o turno de realização dessa tarefa: ");
+    scanf("%10", turno);
+    printf("\n");
+    printf("Informe a prioridade dessa tarefa: ");
+    scanf("%18", prioridade);
+    printf("\n");
+    printf("Informe o status dessa tarefa: ");
+    scanf("%10", status);
 }
 
 void exibir_tarefas(void) { // Esta tela antecede a tela que exibirá os dados das tarefas
@@ -51,7 +75,7 @@ void excluir_tarefas(void) { // Esta tela antece a tela que exibirá os dados da
 void dados_tarefas(void) { // será implementado as variveis de cada dado
     printf("\n");
     printf("|-----------------------------------------------------------------------------------------------------------------------------------------------|\n");
-    printf("|                                                               Dados da Tarefa                                                                 |\n");
+    printf("|                                                            Dados da Tarefa                                                                    |\n");
     printf("|-----------------------------------------------------------------------------------------------------------------------------------------------|\n");
     printf("|   ID   |          Título          |                   Descrição                   |    Data   |    Turno   |     Prioridade    |    Status    |\n");
     printf("|-----------------------------------------------------------------------------------------------------------------------------------------------|\n");
@@ -61,7 +85,7 @@ void dados_tarefas(void) { // será implementado as variveis de cada dado
 void alterar_dados_tarefas(void) {
     printf("\n");
     printf("|-----------------------------------------------------------------------------------------------------------------------------------------------|\n");
-    printf("|                                                            Alterar Dados da Tarefa                                                            |\n");
+    printf("|                                                        Alterar Dados da Tarefa                                                                |\n");
     printf("|-----------------------------------------------------------------------------------------------------------------------------------------------|\n");
     printf("|   ID   |    1   ->   Título       |             2   ->   Descrição                | 3 -> Data | 4 -> Turno |  5 -> Prioridade  | 6 -> Status  |\n");
     printf("|-----------------------------------------------------------------------------------------------------------------------------------------------|\n");
