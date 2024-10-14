5251b802e74ca26546c498c9053e15bc4b7a0fd6
André Leandro <speykergamer@gmail.com>
2024-10-14 11:26:38 -0300

    feat: cria sistema de navegação pelos módulos de usuário #36

---
+++ Usuarios/NavigationUser.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "UsuariosView.h"
+#include "../libs/utils.h"
+
+void navegacao_usuarios(void){
+    char op;
+    do{
+        op = menu_usuarios();
+        switch(op){
+            case '1':  
+                cadastrar_usuarios();
+                limpa_buffer();
+                break;
+            case '2':
+                exibir_dados_usuario();
+                limpa_buffer();
+                break;
+            case '3':
+                alterar_dados_usuario();
+                limpa_buffer();
+                break;
+            case '4':
+                excluir_usuario();
+                limpa_buffer();
+                break;
+            case '0':
+                printf("Saindo...\n");
+                break;
+            default:
+                printf("Opção inválida, por favor, digite outro valor\n");
+                break;
+        }
+            
+
+    } while (op != '0');
+}   
