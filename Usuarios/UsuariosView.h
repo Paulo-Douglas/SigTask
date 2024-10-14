e74c765c54bcc2376973dd13d82280562e365068
PauloDev <paulodouglasmdias@gmail.com>
2024-10-14 12:39:47 -0300

    feat: adiciona as assinaturas das telas de exibir e alterar dados dos usuários. Closes #38

--- Usuarios/UsuariosView.h
+++ Usuarios/UsuariosView.h
@@ -6,5 +6,7 @@ void cadastrar_usuarios(void);
 void exibir_dados_usuario(void);
 void alterar_dados_usuario(void);
 void excluir_usuario(void);
+void dados_usuario(void);
+void alterar_dados_usuario(void);
 
 #endif
