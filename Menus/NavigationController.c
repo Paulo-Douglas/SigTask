b1bb4e55c133b36ad036ca36d8d3eb6f94608331
André Leandro <speykergamer@gmail.com>
2024-10-14 22:13:07 -0300

    fix: alteração para a função correta remetente a função de navegação de equipes #53

--- Menus/NavigationController.c
+++ Menus/NavigationController.c
@@ -3,6 +3,8 @@
 #include "../Usuarios/NavigationUser.h"
 #include "../Tarefas/TarefasView.h"
 #include "../Compromissos/CompromissosView.h"
+#include "../Equipes/EquipesView.h"
+#include "../Equipes/NavigationTeams.h"
 
 void navegacao_modulos(void) {
     char op;    
@@ -14,7 +16,7 @@ void navegacao_modulos(void) {
                 limpa_buffer();
                 break;
             case '2':
-                alterar_dados_compromissos(); // colocar o de equipes aqui
+                navegacao_times(); // colocar o de equipes aqui
                 limpa_buffer();
                 break;
             case '3':
