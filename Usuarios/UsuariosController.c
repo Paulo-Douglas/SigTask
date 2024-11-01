dadc37dd69580d873c45a3245fd7234c044d4814
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-11-01 09:00:11 -0300

    feat: create controller para usuarios

---
+++ Usuarios/UsuariosController.c
@@ -0,0 +1,14 @@
+#include "../libs/utils.h"
+#include "../libs/inputs.h"
+
+void controller_user(void) {
+    // printf("Digite seu nome: ");
+    // scanf("%[A-Za-z ]", nome);
+    // limpa_buffer();
+    // printf("\n");
+    // printf("Digite seu CPF: ");
+    // scanf("%[0-9.-]", cpf);
+    // limpa_buffer();
+    printf("\n");
+    readPhone();
+}
---
+++ Usuarios/UsuariosController.h
@@ -0,0 +1,12 @@
+#ifndef USUARIOSCONTROLLER_H
+#define USUARIOSCONTROLLER_H
+
+typedef struct
+{
+    char nome[50];
+    char cpf[12];
+    char telefone[15];
+}User;
+
+
+#endif // USUARIOSCONTROLLER_H
