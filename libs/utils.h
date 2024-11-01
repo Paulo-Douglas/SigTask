3da99b5b1c439f2b4c2296c72b56e84681c86477
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-11-01 14:11:46 -0300

    feat: add tamanhos de descrições

--- libs/utils.h
+++ libs/utils.h
@@ -3,9 +3,11 @@
 
 #define TRUE 1
 #define FALSE 0
-#define MAX_NAME_LENGTH 50
+#define MAX_NAME_LENGTH 51
 #define MAX_CPF_LENGTH 12
 #define MAX_TEL_LENGTH 12
+#define MAX_DESCRIPTION_LENGTH 51
+#define MIN_DESCRIPTION_LENGTH 10
 
 void limpa_buffer(void);
 void exibir_menssagem(char *);
