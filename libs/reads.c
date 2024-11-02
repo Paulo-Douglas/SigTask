a340b51997f012698ac5a9c9bfa8f9bd3c7cb721
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-11-02 17:23:13 -0300

    refactor: modificando função para ler string

--- libs/reads.c
+++ libs/reads.c
@@ -7,8 +7,7 @@
 #include "utils.h"
 
 
-
-void read_title(void) {
+void read_string(void) {
     limpa_buffer();
     char title[MAX_NAME_LENGTH];
 
@@ -18,45 +17,10 @@ void read_title(void) {
         printf("\n");
 
         if (!valid_name(title)) {
-            show_error("Título inválido ou muito longo (maior que 50 caracteres)");
-            limpa_buffer();
+            show_error("Inválido ou muito longo (maior que 50 caracteres)");            limpa_buffer();
             printf("Tente novamente: ");
         }
     } while(valid_name(title) == FALSE);
-}
-
-void read_phone(void){
-    char phone[MAX_TEL_LENGTH];
-    
-    do{
-        fgets(phone, MAX_TEL_LENGTH, stdin);
-        phone[strcspn(phone, "\n")] = 0;
-        printf("\n");
-
-        if(!valid_phone(phone)){
-            show_error("Número de telefone inválido: (XX X XXXXXXXX)");
-            limpa_buffer();
-            printf("Tente novamente: ");
-        }
-
-    } while(valid_phone(phone) == FALSE);
-}
-
-void read_name(void){
-    char name[MAX_NAME_LENGTH];
-
-    do{
-        fgets(name, MAX_NAME_LENGTH, stdin);
-        name[strcspn(name, "\n")] = 0;
-        printf("\n");
-
-        if(!valid_name(name)){
-            show_error("Nome inválido ou muito longo (maior que 50 caracteres)");
-            limpa_buffer();
-            printf("Tente novamente: ");
-        }
-
-    } while(valid_name(name) == FALSE);
 }
 
 void read_cpf(void){
--- libs/reads.h
+++ libs/reads.h
@@ -1,9 +1,8 @@
 #ifndef READS_H
 #define READS_H
 
-void read_title(void);
+void read_string(void);
 void read_phone(void);
-void read_name(void);
 void read_cpf(void);
 void read_description(void);
 void read_date(void);
