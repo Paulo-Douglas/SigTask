7e3259e4b1ffc4edf8e50026d0fe48d5af620385
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-11-02 06:11:29 -0300

    feat: add assinaturas das funções

--- libs/validate.h
+++ libs/validate.h
@@ -5,5 +5,9 @@ int valid_phone(char *telephone);
 int length_prompt(char *prompt, int maxLength, int minLength);
 int valid_name(char *name);
 int valid_description(char *description);
+int validation_cpf(char cpf[]);
+int bissexto(char year[]);
+int dia_mes(int mês);
+int validar_data(char day[], char month[], char year[]);
 
 #endif
