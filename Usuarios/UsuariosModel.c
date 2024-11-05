7c3747477bd79c0f37707609af64ae198917fb4b
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-11-05 07:55:02 -0300

    feat: add função para verificar se cpf existe

--- Usuarios/UsuariosModel.c
+++ Usuarios/UsuariosModel.c
@@ -7,7 +7,36 @@
 
 #include "UsuariosModel.h"
 
-void save_user(char *name, char *cpf, char *phone){
+int cpf_unique_user(const char *cpf){
+    FILE *fp = fopen("data/users.txt", "r");
+    if(fp == NULL){
+        return 0;
+    }
+
+    char line[MAX_CPF_LENGTH + MAX_NAME_LENGTH + MAX_TEL_LENGTH + 3];
+    char cpf_user[MAX_CPF_LENGTH];
+
+    while (fgets(line, sizeof(line), fp)) {
+        sscanf(line, "%14[^,]", cpf_user);
+
+        // Comparação de vetores
+        if (strcmp(cpf, cpf_user) == 0) {
+            fclose(fp);
+            return TRUE;
+        }
+
+    }
+
+    fclose(fp);
+    return FALSE;
+
+}
+
+int save_user(char *name, char *cpf, char *phone){
+
+    if(cpf_unique_user(cpf)){
+        return FALSE;
+    }
 
     struct stat path = {0};
     if(stat("data", &path) == -1){
@@ -22,6 +51,9 @@ void save_user(char *name, char *cpf, char *phone){
 
     FILE *fp;
     fp = fopen("data/users.txt", "a");
-    fprintf(fp, "%s,%s,%s\n", usario.name, usario.cpf, usario.phone);
+    fprintf(fp, "%s,%s,%s\n", usario.cpf, usario.name, usario.phone);
     fclose(fp);
+
+    return TRUE;
+
 }
