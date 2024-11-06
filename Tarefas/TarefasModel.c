aa7ea3843c617995302bffc5f7357e42bf4706e4
André Leandro <speykergamer@gmail.com>
2024-11-06 18:06:39 -0300

    refactor: teste de implementações esquecidas

--- Tarefas/TarefasModel.c
+++ Tarefas/TarefasModel.c
@@ -5,5 +5,16 @@
 #include "../libs/validate.h"
 #include "../libs/utils.h"
 
+int save_task(char *cpf,char *title, char *description, char *day, char *month, 
+              char *year, char *turn, char *priority, char *status){
+
+
+
+    if (cpf_unique_user(cpf, "data/users.txt")){
+        return FALSE;
+    }
+
+              }
+
 
 
