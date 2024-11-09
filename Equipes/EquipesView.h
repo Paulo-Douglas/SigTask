535aeeb60ae67e3ae70f0388ee360b4868911c1e
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-11-09 18:45:11 -0300

    refactor: add função de remover e add membro

--- Equipes/EquipesView.h
+++ Equipes/EquipesView.h
@@ -6,9 +6,10 @@ void cadastro_tipo_de_equipe(void);
 void cadastrar_equipe_academica(void);
 void cadastrar_equipe_empresarial(void);
 void exibir_equipe(void);
-void editar_equipe(void);
 void excluir_equipe(void);
 void alterar_equipe_academica(void);
 void alterar_equipe_empresarial(void);
+void add_user_to_team(void);
+void remove_user_from_team(void);
 
 #endif
