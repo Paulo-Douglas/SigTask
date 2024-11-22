302c2e6cc9df2c4d5bbd2db459bed443b5c32a82
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-11-22 18:17:23 -0300

    feat: função para exibir o cabeçalho

--- libs/styles.c
+++ libs/styles.c
@@ -9,3 +9,10 @@ void show_error(const char *message) {
 void show_sucess(const char *message) {
     printf("\033[1;32m[SUCESSO] %s\033[0m\n", message);
 }
+
+void show_header(const char *title) {
+    limpar_tela();
+    printf("|-------------------------------------------------------------------------------------------------------|\n");
+    printf("|%*s%*s|\n", (int)(50 + strlen(title) / 2), title, (int)(49 - strlen(title) / 2), "");
+    printf("|-------------------------------------------------------------------------------------------------------|\n");
+}
