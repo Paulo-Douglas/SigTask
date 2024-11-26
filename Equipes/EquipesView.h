81e39e77ef263569a8075aa14433490066e3f6f9
André Leandro <speykergamer@gmail.com>
2024-11-26 16:22:28 -0300

    fix: colocando os novos nomes das telas

--- Equipes/EquipesView.h
+++ Equipes/EquipesView.h
@@ -7,8 +7,9 @@ void cadastrar_equipe_academica(void);
 void cadastrar_equipe_empresarial(void);
 void exibir_equipe(void);
 void excluir_equipe(void);
-void alterar_equipe_academica(void);
-void alterar_equipe_empresarial(void);
+void alterar_tipo_de_equipe(void);
+void change_data_teams_academic(void);
+void change_data_teams_business(void);
 void add_user_to_team(void);
 void remove_user_from_team(void);
 
