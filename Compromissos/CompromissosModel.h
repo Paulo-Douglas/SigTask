9c42d6e0328b0161e39eeb9a3ee7e00205a1351e
André Leandro <speykergamer@gmail.com>
2024-12-02 05:45:40 -0300

    fix: ajustando o cabeçalho de compromissosmodel

--- Compromissos/CompromissosModel.h
+++ Compromissos/CompromissosModel.h
@@ -16,8 +16,8 @@ typedef struct
 
 } Compromisers;
 
-int load_compromise(Compromisers *compromise, const char *id, const char *mod);
+int load_compromise(Compromisers *compromise, const char *id);
 int insert_compromise(Compromisers *compromise);
-int update_date_compromise(const char delimit, const char *new_data,  const int lenght, int id);
+int update_date_compromise(const char **id, const char *new_value, const char *field, int length);
 
 #endif
