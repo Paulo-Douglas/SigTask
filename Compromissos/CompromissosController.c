e11ba181e445a71da3bdd2e9cf20374e78b7f847
André Leandro <speykergamer@gmail.com>
2024-12-10 15:50:55 -0300

    refactor: ajuste nas variáveis do struct nas funções e função de buscar compromisso de equipe

--- Compromissos/CompromissosController.c
+++ Compromissos/CompromissosController.c
@@ -19,26 +19,17 @@ int register_compromise(void){
     Compromisers compromise = {NULL, NULL, NULL, NULL, NULL, NULL};
     int year = year_now();
 
-    read_and_assign(&compromise.team_id, "|\tID: ", read_id);
-    read_and_assign(&compromise.title, "|\tTítulo: ", read_string);
-    read_and_assign(&compromise.description, "|\tDescrição: ", read_string);
+    read_and_assign(&compromise.id, "|\tID: ", read_id);
+    read_and_assign(&compromise.name_anything, "|\tTítulo: ", read_string);
+    read_and_assign(&compromise.situation, "|\tSituação: ", read_string);
+    read_and_assign(&compromise.description, "|\tDescrição: ", read_description);
 
-    char day_start[MAX_DAY_LENGHT];
-    char month_start[MAX_MONTH_LENGHT];
-    printf("\t DATA INICIAL\n");
-    read_date(day_start, month_start);
-    compromise.start_date = malloc(sizeof(char) * 12);
-    snprintf(compromise.start_date, 12, "%s/%s/%d", day_start, month_start, year);
-
-    char day_end[MAX_DAY_LENGHT];
-    char month_end[MAX_MONTH_LENGHT];
-    printf("\t DATA FINAL\n");
-    read_date(day_end, month_end);
-    compromise.end_date = malloc(sizeof(char) * 12);
-    snprintf(compromise.end_date, 12, "%s/%s/%d", day_end, month_end, year);
-
-    read_and_assign(&compromise.time, "|\tHorário: ", read_time);
-    read_and_assign(&compromise.priority, "|\tPrioridade: (1) Alta | (2) Media | (3) Baixa: ", read_generic_123);
+    char day[MAX_DAY_LENGHT];
+    char month[MAX_MONTH_LENGHT];
+    printf("\tData: \n");
+    read_date(day, month);
+    compromise.date = malloc(sizeof(char) * 12);
+    snprintf(compromise.date, 12, "%s/%s/%d", day, month, year);
 
     int result = insert_compromise(&compromise);
     free_struct_compromise(&compromise);
@@ -47,9 +38,12 @@ int register_compromise(void){
 
 
 int search_compromiser_to_user(const char* cpf){
-    return cpf_unique_user(cpf, "data/users.txt");
+    return cpf_exists(cpf);
 }
 
+int search_compromiser_to_team(const char* id){
+    return id_exists(id);
+}
 
 int update_compromise(Compromisers *compromise, const char *id) {
     limpar_tela();
@@ -65,49 +59,35 @@ int update_compromise(Compromisers *compromise, const char *id) {
             case '1':
                 limpa_buffer();
                 printf("|\tTítulo: ");
-                compromise->title = read_string();
-                return update_date_compromise(&id, compromise->title, FIELD_TITLE, VARCHAR50);
+                compromise->name_anything = read_string();
+                return update_date_compromise(&id, compromise->name_anything, FIELD_TITLE, VARCHAR50);
 
             case '2':
                 limpa_buffer();
-                printf("|\tDescrição: ");
-                compromise->description = read_description();
-                return update_date_compromise(&id, compromise->description, FIELD_DESCRIPTION, VARCHAR50);
+                printf("|\tSituação: ");
+                compromise->situation = read_string();
+                return update_date_compromise(&id, compromise->situation, FIELD_DESCRIPTION, VARCHAR50);
                 
 
             case '3':
                 limpa_buffer();
-                printf("\t DATA INICIAL\n");
-                free(compromise->start_date);
-                compromise->start_date = read_and_format_date(year);
-                return update_date_compromise(&id, compromise->start_date, FIELD_DATE, VARCHAR50);
-
+                printf("\tData: \n");
+                free(compromise->date);
+                compromise->date = read_and_format_date(year);
+                return update_date_compromise(&id, compromise->date, FIELD_DATE, VARCHAR50);
+            
             case '4':
                 limpa_buffer();
-                printf("\t DATA FINAL\n");
-                free(compromise->end_date);
-                compromise->end_date = read_and_format_date(year);
-                return update_date_compromise(&id, compromise->end_date, FIELD_DATE, VARCHAR50);
+                printf("|\tDescrição: ");
+                compromise->description = read_description();
+                return update_date_compromise(&id, compromise->description, FIELD_DESCRIPTION, VARCHAR50);
 
             case '5':
-                limpa_buffer();
-                printf("|\tHorário: ");
-                compromise->time = read_time();
-                return update_date_compromise(&id, compromise->time, FIELD_TIME, VARCHAR50);
-                
-
-            case '6':
-                limpa_buffer();
-                printf("|\tPrioridade: (1) Alta | (2) Media | (3) Baixa: ");
-                compromise->priority = read_generic_123("priority");
-                return update_date_compromise(&id, compromise->priority, FIELD_PRIORITY, VARCHAR50);
-
-            case '7':
                 limpa_buffer();
                 if(strcmp(compromise->status, "0") == 0) {show_error("| Compromisso ja fechado!\n"); return FALSE;}
                 return update_date_compromise(&id, "0", FIELD_STATUS, 1);
                 
-            case '8':
+            case '6':
                 limpa_buffer();
                 if(strcmp(compromise->status, "1") == 0) {show_error("| Compromisso ja aberto!\n"); return FALSE;}
                 return update_date_compromise(&id, "1", FIELD_STATUS, 1);
@@ -122,11 +102,9 @@ int update_compromise(Compromisers *compromise, const char *id) {
 
 
 void free_struct_compromise(Compromisers *compromise){
-    if(compromise->title != NULL) free(compromise->title);
-    if(compromise->description != NULL) free(compromise->description);
-    if(compromise->start_date != NULL) free(compromise->start_date);
-    if(compromise->end_date != NULL) free(compromise->end_date);
-    if(compromise->time != NULL) free(compromise->time);
-    if(compromise->priority != NULL) free(compromise->priority);
+    if(compromise->name_anything != NULL) free(compromise->name_anything);
+    if(compromise->situation != NULL) free(compromise->situation);
+    if(compromise->date != NULL) free(compromise->date);
+    if(compromise->description != NULL) free (compromise->description);
 }
 
