773ee70e1061dc38e0318b4e22c86883034f0369
andersonstack <andersong.pereiracruz@gmail.com>
2025-01-15 15:13:15 -0300

    feat: add enum de status

--- libs/utils.h
+++ libs/utils.h
@@ -11,21 +11,25 @@
 #define MAX_MONTH_LENGHT 3
 #define MAX_LINE_LENGTH 250
 
-
-typedef struct {
+typedef struct
+{
   char **file;
   int line_count;
 } File;
 
+typedef enum
+{
+  DESATIVADO,
+  ATIVADO
+} Status;
 
 void limpa_buffer(void);
-void limpar_tela(void); 
+void limpar_tela(void);
 void create_path(const char *name_path);
 void enter(void);
 
 int check_dates(const char *date1);
 int year_now(void);
-void date_now(char *date) ;
-
+void date_now(char *date);
 
 #endif
