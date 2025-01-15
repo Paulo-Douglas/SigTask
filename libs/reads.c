ea9ac10ff3e7a1f7b3c3482d84ef7380033e0210
andersonstack <andersong.pereiracruz@gmail.com>
2025-01-15 16:05:32 -0300

    style: ajustando layout do código

--- libs/reads.c
+++ libs/reads.c
@@ -9,15 +9,15 @@
 #include "utils.h"
 #include "date.h"
 
-
-void read_and_assign(char **field, const char *prompt, char *(*read_function)()) {
+void read_and_assign(char **field, const char *prompt, char *(*read_function)())
+{
     printf("%s", prompt);
     free(*field);
     *field = read_function();
 }
 
-
-void input(char **prompt) {
+void input(char **prompt)
+{
     char line[MAX_LINE_LENGTH];
 
     // Lê a entrada do usuário
@@ -26,99 +26,113 @@ void input(char **prompt) {
 
     // Aloca memória para a string e copia o conteúdo
     *prompt = (char *)malloc(strlen(line) + 1);
-    if (*prompt == NULL) {
+    if (*prompt == NULL)
+    {
         perror("Erro ao alocar memória");
         exit(EXIT_FAILURE);
     }
     strcpy(*prompt, line);
 }
 
-
-char* read_string(void) {
+char *read_string(void)
+{
     char *string;
     int is_valid;
 
-    do {
+    do
+    {
         input(&string);
         is_valid = validate_name(string);
 
-        if (!is_valid) {
-            show_error("[ERROR] Entrada inválida!");            
+        if (!is_valid)
+        {
+            show_error("[ERROR] Entrada inválida!");
             printf("Tente novamente: ");
             free(string);
         }
-    } while(!is_valid);
+    } while (!is_valid);
 
     limpa_buffer();
     return string;
 }
 
-
-char* read_phone(void){
+char *read_phone(void)
+{
     char *phone;
 
-    do{
+    do
+    {
         input(&phone);
 
-        if(!valid_phone(phone)){
+        if (!valid_phone(phone))
+        {
             show_error("Número de telefone inválido: (XX X XXXXXXXX)");
             printf("Tente novamente: ");
             free(phone);
         }
 
-    } while(valid_phone(phone) == FALSE);
+    } while (valid_phone(phone) == FALSE);
     limpa_buffer();
     return phone;
 }
 
-
-char* read_cpf(void){
+char *read_cpf(void)
+{
     char *cpf = NULL;
 
-    do{
-        if (cpf != NULL){
+    do
+    {
+        if (cpf != NULL)
+        {
             free(cpf);
         }
 
         input(&cpf);
 
-        if (!validate_cpf(cpf)){
+        printf("CPF LIDO %s\n", cpf);
+
+        if (!validate_cpf(cpf))
+        {
             show_error("CPF inválido (Formato correto: XXX.XXX.XXX-XX)");
             printf("Tente novamente: ");
         }
 
-    } while(!validate_cpf(cpf));
+    } while (!validate_cpf(cpf));
 
     limpa_buffer();
     return cpf;
 }
 
-char* read_description(void) {
+char *read_description(void)
+{
     char *description;
     int is_valid;
-    
-    do {
+
+    do
+    {
         input(&description);
         is_valid = validate_description(description);
 
-        if (!is_valid){
-            show_error("[ERROR] Entrada inválida!");  
+        if (!is_valid)
+        {
+            show_error("[ERROR] Entrada inválida!");
             printf("Tente novamente ");
             free(description);
-        
         }
-    } while(!is_valid);
+    } while (!is_valid);
     limpa_buffer();
     return description;
 }
 
-
-char read_generic_123(const char *dir) {
+char read_generic_123(const char *dir)
+{
     char *prompt = NULL;
-    do {
+    do
+    {
         input(&prompt);
 
-        if (prompt == NULL || *prompt < '1' || *prompt > '3') {
+        if (prompt == NULL || *prompt < '1' || *prompt > '3')
+        {
             show_error("É preciso digitar uma opção: 1, 2 ou 3\n");
             printf("Tente novamente: ");
             free(prompt);
@@ -127,28 +141,48 @@ char read_generic_123(const char *dir) {
     } while (prompt == NULL || *prompt < '1' || *prompt > '3');
 
     char result;
-    if (strcmp(dir, "turn") == 0) {
-        if (strcmp(prompt, "1") == 0) {
+    if (strcmp(dir, "turn") == 0)
+    {
+        if (strcmp(prompt, "1") == 0)
+        {
             result = 'M';
-        } else if (strcmp(prompt, "2") == 0) {
+        }
+        else if (strcmp(prompt, "2") == 0)
+        {
             result = 'V';
-        } else {
+        }
+        else
+        {
             result = 'N';
         }
-    } else if (strcmp(dir, "priority") == 0) {
-        if (strcmp(prompt, "1") == 0) {
+    }
+    else if (strcmp(dir, "priority") == 0)
+    {
+        if (strcmp(prompt, "1") == 0)
+        {
             result = 'A';
-        } else if (strcmp(prompt, "2") == 0) {
+        }
+        else if (strcmp(prompt, "2") == 0)
+        {
             result = 'M';
-        } else {
+        }
+        else
+        {
             result = 'B';
         }
-    } else if (strcmp(dir, "type") == 0) {
-        if (strcmp(prompt, "1") == 0) {
+    }
+    else if (strcmp(dir, "type") == 0)
+    {
+        if (strcmp(prompt, "1") == 0)
+        {
             result = 'E';
-        } else if (strcmp(prompt, "2") == 0) {
+        }
+        else if (strcmp(prompt, "2") == 0)
+        {
             result = 'R';
-        } else {
+        }
+        else
+        {
             result = 'P';
         }
     }
--- libs/validate.c
+++ libs/validate.c
@@ -5,19 +5,19 @@
 #include "utils.h"
 #include "validate.h"
 
-
 #include "../Usuarios/UsuariosController.h"
 #include "../Equipes/EquipesController.h"
 
-
-int valid_phone(char *telephone) {
+int valid_phone(char *telephone)
+{
     regex_t phoneRegex;
 
     const char *standardPhone = "^[0-9]{2} [0-9]{1} [0-9]{8}$";
     int reti;
 
     reti = regcomp(&phoneRegex, standardPhone, REG_EXTENDED);
-    if (reti) return FALSE;
+    if (reti)
+        return FALSE;
 
     reti = regexec(&phoneRegex, telephone, 0, NULL, 0);
     regfree(&phoneRegex);
@@ -25,15 +25,16 @@ int valid_phone(char *telephone) {
     return reti == 0;
 }
 
-
-int validate_name(char *name) {
+int validate_name(char *name)
+{
     regex_t nameRegex;
 
     const char *standardName = "^[A-Za-zÇçÁÂÉÊÍÓÔÚáâéêíóôú]+( [A-Za-zÇçÁÂÉÊÍÓÔÚáâéêíóôú]+)*$";
     int reti;
 
     reti = regcomp(&nameRegex, standardName, REG_EXTENDED);
-    if(reti) return FALSE;
+    if (reti)
+        return FALSE;
 
     reti = regexec(&nameRegex, name, 0, NULL, 0);
     regfree(&nameRegex);
@@ -41,19 +42,23 @@ int validate_name(char *name) {
     return reti == 0;
 }
 
-int length_prompt(char *prompt, int minLength) {
+int length_prompt(char *prompt, int minLength)
+{
     return strlen(prompt) < minLength;
 }
 
-int validate_description(char *description) {
-    if(length_prompt(description, MIN_DESCRIPTION_LENGTH)) return 0;
+int validate_description(char *description)
+{
+    if (length_prompt(description, MIN_DESCRIPTION_LENGTH))
+        return 0;
 
     regex_t descriptionRegex;
-    const char *standardDescription = "^[A-Za-zÇçÁÂÉÊÍÓÔÚáâéêíóôú.,!?-]+( [A-Za-zÇçÁÂÉÊÍÓÔÚáâéêíóôú.,!?-]+)*$";    
+    const char *standardDescription = "^[A-Za-zÇçÁÂÉÊÍÓÔÚáâéêíóôú.,!?-]+( [A-Za-zÇçÁÂÉÊÍÓÔÚáâéêíóôú.,!?-]+)*$";
     int reti;
 
     reti = regcomp(&descriptionRegex, standardDescription, REG_EXTENDED);
-    if(reti){
+    if (reti)
+    {
         return FALSE;
     }
 
@@ -63,45 +68,51 @@ int validate_description(char *description) {
     return reti == 0;
 }
 
-int validate_cpf(char cpf[]) {  
-        int calcule_one_dig = 0;  
-        int calcule_second_dig = 0;
-        int x = 14;
-        
-        calcule_one_dig  = (cpf[0] - '0') * 10 + (cpf[1] - '0') * 9 + 
-                (cpf[2] - '0') * 8 + (cpf[4] - '0') * 7 +                               // aqui faço pulo de indices para dar espaço 
-                (cpf[5] - '0') * 6 + (cpf[6] - '0') * 5 +                               // para os indices 3, 7, 11 (para pontuação)
-                (cpf[8] - '0') * 4 + (cpf[9] - '0') * 3 + 
-                (cpf[10] - '0') * 2;
+int validate_cpf(char cpf[])
+{
+    int calcule_one_dig = 0;
+    int calcule_second_dig = 0;
+    int x = 14;
 
+    calcule_one_dig = (cpf[0] - '0') * 10 + (cpf[1] - '0') * 9 +
+                      (cpf[2] - '0') * 8 + (cpf[4] - '0') * 7 + // aqui faço pulo de indices para dar espaço
+                      (cpf[5] - '0') * 6 + (cpf[6] - '0') * 5 + // para os indices 3, 7, 11 (para pontuação)
+                      (cpf[8] - '0') * 4 + (cpf[9] - '0') * 3 +
+                      (cpf[10] - '0') * 2;
 
-        calcule_second_dig = (cpf[0] - '0') * 11 + (cpf[1] - '0') * 10 + 
-                (cpf[2] - '0') * 9 + (cpf[4] - '0') * 8 + 
-                (cpf[5] - '0') * 7 + (cpf[6] - '0') * 6 + 
-                (cpf[8] - '0') * 5 + (cpf[9] - '0') * 4 + 
-                (cpf[10] - '0') * 3 + (cpf[12] - '0') * 2;
-        
-        if (((calcule_one_dig  * 10) % 11 == (cpf[12] - '0')) &&  (calcule_second_dig * 10) % 11 == (cpf[13] - '0') && (strlen(cpf) >= x)
-            && cpf[3] == '.' && cpf[7] == '.' && cpf[11] == '-') {
-            return 1;
-        }    
-        else {
-            return 0;
-    }   
+    calcule_second_dig = (cpf[0] - '0') * 11 + (cpf[1] - '0') * 10 +
+                         (cpf[2] - '0') * 9 + (cpf[4] - '0') * 8 +
+                         (cpf[5] - '0') * 7 + (cpf[6] - '0') * 6 +
+                         (cpf[8] - '0') * 5 + (cpf[9] - '0') * 4 +
+                         (cpf[10] - '0') * 3 + (cpf[12] - '0') * 2;
+
+    if (((calcule_one_dig * 10) % 11 == (cpf[12] - '0')) && (calcule_second_dig * 10) % 11 == (cpf[13] - '0') && (strlen(cpf) >= x) && cpf[3] == '.' && cpf[7] == '.' && cpf[11] == '-')
+    {
+        return 1;
     }
+    else
+    {
+        return 0;
+    }
+}
 
-
-int user_exists(const char *key) {
+int user_exists(const char *key)
+{
     int exists = FALSE;
 
     FILE *fp = fopen("data/users.dat", "rb");
-    if (fp == NULL) return exists;
+    if (fp == NULL)
+        return exists;
 
-    User *user = (User*)malloc(sizeof(User));
-    while (fread(user, sizeof(User), 1, fp) && !exists){
-        if ((strcmp(user->cpf, key) == 0) && (strlen(key) > 5)){
+    User *user = (User *)malloc(sizeof(User));
+    while (fread(user, sizeof(User), 1, fp) && !exists)
+    {
+        if ((strcmp(user->cpf, key) == 0) && (strlen(key) > 5))
+        {
             exists = TRUE;
-        } else if ((strcmp(user->id, key) == 0) && (user->status == '1')){
+        }
+        else if ((strcmp(user->id, key) == 0) && (user->status == '1'))
+        {
             exists = TRUE;
         }
     }
@@ -110,15 +121,19 @@ int user_exists(const char *key) {
     return exists;
 }
 
-int team_exists(const char *key) {
+int team_exists(const char *key)
+{
     int exists = FALSE;
 
     FILE *fp = fopen("data/teams.dat", "rb");
-    if (fp == NULL) return exists;
+    if (fp == NULL)
+        return exists;
 
-    Team *team = (Team*)malloc(sizeof(Team));
-    while (fread(team, sizeof(Team), 1, fp) && !exists){
-        if ((strcmp(team->id, key) == 0) && (team->status == '1')){
+    Team *team = (Team *)malloc(sizeof(Team));
+    while (fread(team, sizeof(Team), 1, fp) && !exists)
+    {
+        if ((strcmp(team->id, key) == 0) && (team->status == '1'))
+        {
             exists = TRUE;
         }
     }
